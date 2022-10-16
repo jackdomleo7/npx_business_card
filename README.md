ae9a6bbdf5d53e75198e0475cdde5cd8c8f45ce9
Jack Domleo <jackdomleo7@gmail.com>
2022-10-16 18:03:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+
+[![Pipeline](https://github.com/jackdomleo7/npx_business_card/actions/workflows/pipeline.yml/badge.svg?branch=main)](https://github.com/jackdomleo7/npx_business_card/actions/workflows/pipeline.yml) ![MIT License](https://img.shields.io/badge/License-MIT-important) [![npm](https://img.shields.io/npm/dt/jackdomleo7)](https://www.npmjs.com/package/jackdomleo7) [![GitHub release (latest by date)](https://img.shields.io/github/v/release/jackdomleo7/npx_business_card)](https://github.com/jackdomleo7/npx_business_card/releases)
+
+</div>
+
+# npx Business Card
+
+```cli
+npx jackdomleo7
+```
+
+## Development
+
+Install dependencies
+```cli
+npm i
+```
+
+Build the project for production
+```cli
+npm run build
+```
+
+Run the production files
+```cli
+npm run start
+```
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import boxen from 'boxen'
+import chalk from 'chalk'
+import splitText from 'split-text'
+import data from './data.json'
+
+console.log(boxen([
+  chalk.hex('#e69e6b').bold(data.name),
+  chalk.grey(data.urls.personal),
+  '',
+  `${chalk.white(data.occupation.title)} at ${chalk.white(data.occupation.company)}`,
+  '',
+  `Twitter: ${chalk.grey(`https://twitter.com/${data.social.twitter}`)}`,
+  `LinkedIn: ${chalk.grey(`https://linkedin.com/in/${data.social.linkedin}`)}`,
+  `GitHub: ${chalk.grey(`https://github.com/${data.social.github}`)}`,
+  `CodePen: ${chalk.grey(`https://codepen.io/${data.social.codepen}`)}`,
+  '',
+  ...splitText(data.message, 50).map(text => chalk.italic(text))
+].join('\n'), {
+  margin: 1,
+  padding: 1,
+  float: 'center',
+  borderStyle: 'round',
+  borderColor: '#f75c20'
+}))
